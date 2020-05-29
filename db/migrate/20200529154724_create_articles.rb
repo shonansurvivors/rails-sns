bc7f19b9aec4ce1878e324f3c74399e90c0d5cb1
shonansurvivors <shonansurvivors@gmail.com>
2020-05-30 01:11:26 +0900

    articlesテーブル定義

--- db/migrate/20200529154724_create_articles.rb
+++ db/migrate/20200529154724_create_articles.rb
@@ -1,7 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-
+      t.string :title, null: false, default: ""
+      t.text :body, null: false, default: ""
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
